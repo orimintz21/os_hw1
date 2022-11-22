ab4906bcc7db4fe3a75360d010016bb2de69c9ad
almogkarif <almogkarif@gmail.com>
2022-11-22 19:32:35 +0200

    no error

--- Commands.h
+++ Commands.h
@@ -228,6 +228,7 @@ class KillCommand : public BuiltInCommand
   // TODO: Add your data members
 public:
   KillCommand(const char *cmd_line, JobsList *jobs);
+  //KillCommand(const char *cmd_line, JobsList *jobs);
   virtual ~KillCommand() {}
   void execute() override;
 };
